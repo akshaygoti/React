b01a6df0719e6091202b8022e737e7020c6d4728
Akshay <akshaygoti23@gmail.com>
2024-03-22 08:51:28 +0530

    22-03-2024

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import Pera from "./Componets/Pera";
+import "./App.css";
+import "./Componets/Arrow.js"
+
+import {Pera1,Pera2,Pera3,Pera4,Pera5,Pera6} from "./Componets/Pera"
+import Img from './Componets/Img.js';
+import Arrow from "./Componets/Arrow.js";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+   <div className="App"> 
+
+    <h1>hello 1</h1>
+    <h1>hello 2</h1>
+    <h1>hello 3</h1>
+    <h1>hello 4</h1>
+    <h1>hello 5</h1>
+    
+    {/* <Index></Index> */}
+    <Pera1/>
+    <Pera2/>
+    <Pera3/>
+    <Pera4/>
+    <Pera5/>
+    <Pera6/>  
+    <Img/>
+  <Arrow/>
+     </div>
+  </>
 }
 
 export default App;
+
+
---
+++ src/Componets/Pera.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+// function Pera(){
+//     return <p>You can also directly specify the project name and the template you want to use via additional command line options. For example, to scaffold a Vite + Vue project, run:</p>
+// }
+
+function Pera1(){
+
+    return <p>
+        <p>lorem</p>
+        You can also directly specify the project name and the template you want to use via additional command line options. For example, to scaffold a Vite + Vue project, run:</p>
+}
+function Pera2(){
+    return <p>You can also directly specify the project name and the template you want to use via additional command line options. For example, to scaffold a Vite + Vue project, run:</p>
+}
+function Pera3(){
+    return <p>You can also directly specify the project name and the template you want to use via additional command line options. For example, to scaffold a Vite + Vue project, run:</p>
+}
+function Pera4(){
+    return <p>You can also directly specify the project name and the template you want to use via additional command line options. For example, to scaffold a Vite + Vue project, run:</p>
+}
+
+function Pera5(){
+    return <p>You can also directly specify the project name and the template you want to use via additional command line options. For example, to scaffold a Vite + Vue project, run:</p>
+}
+
+function Pera6(){
+    return <ul>
+        <li>menu</li>
+        <li>button</li>
+        <li>number</li>
+        <li>menu</li>
+        <li>ok</li>
+        <li>done</li>
+        <li>button</li>
+        <li>number</li>
+    </ul>
+}
+
+// export default Pera;
+export {Pera1,Pera2,Pera3,Pera4,Pera5,Pera6};
